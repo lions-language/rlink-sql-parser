3c983a10b90b3cc0763463d854ba42391bc54d01
lions-language <lions_language@163.com>
2021-08-25 21:52:46 +0800

    add datatype

--- src/ast/data_type.rs
+++ src/ast/data_type.rs
@@ -11,4 +11,26 @@ pub enum DataType {
     Uuid,
     /// Large character object e.g. CLOB(1000)
     Clob(u64),
+    Binary(u64),
+    Varbinary(u64),
+    Blob(u64),
+    Decimal(Option<u64>, Option<u64>),
+    Float(Option<u64>),
+    TinyInt,
+    SmallInt,
+    Int,
+    BigInt,
+    Real,
+    Double,
+    Boolean,
+    Data,
+    Time,
+    Timestamp,
+    Interval,
+    Regclass,
+    Text,
+    String,
+    Bytea,
+    // Custom(ObjectName)
+    Array(Box<DataType>)
 }
